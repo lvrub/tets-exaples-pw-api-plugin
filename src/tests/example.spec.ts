0d8b4026aa040674579e598a7d17efcf666190c0
Leonid <lvrub@users.noreply.github.com>
2025-04-09 19:15:08 +0300

    updated

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -3,7 +3,7 @@ import { pwApi, test, axiosApi } from 'pw-api-plugin';
 import { APIRequestContext, Page } from '@playwright/test';
 
 
-  test('check healthcheck', async ({ request, page }: { request: APIRequestContext, page: Page }) => {
+test('check healthcheck', async ({ request, page }: { request: APIRequestContext, page: Page }) => {
 
   const responseGet = await pwApi.get({request, page},'https://practice.expandtesting.com/notes/api/health-check')
   expect(responseGet.status()).toBe(200)
@@ -23,13 +23,14 @@ test('check healthcheck axios', async ({page }: { page: Page }) => {
 
 test('create user', async ({ request, page }: { request: APIRequestContext, page: Page }) => {
 
+  const date = new Date().getTime().toString()
   const responsePost = await pwApi.post({request, page},'https://practice.expandtesting.com/notes/api/users/register', {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     form: {
-      name: 'testUser'+ Math.random().toString(10).substring(2,4),
-      email: 'test_a'+ Math.random().toString(10).substring(2,4)+'@i.ua',
+      name: 'testUser'+ date.substring(10,15),
+      email: 'test_a'+ date.substring(10,15)+'@i.ua',
       password: '0444000'
     }
   });
@@ -42,10 +43,11 @@ test('create user', async ({ request, page }: { request: APIRequestContext, page
 });
 
 test('create user axios', async ({ page }: {page: Page }) => {
+ const date = new Date().getTime().toString()
 
   const params = new URLSearchParams();
-  params.append('name', 'testUser'+ Math.random().toString(10).substring(2,4));
-  params.append('email', 'test_a'+ Math.random().toString(10).substring(8,10)+'@i.ua');
+  params.append('name', 'testUser'+ date.substring(10,13));
+  params.append('email', 'test_a'+ date.substring(10,13)+'@i.ua');
   params.append('password', '0444009');
 
   const responsePost = await axiosApi.post({page},'https://practice.expandtesting.com/notes/api/users/register', params, 
